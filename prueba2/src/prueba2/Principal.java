d33dd91ea13956863505a70ec4639e86f5d7f743
ErnestoIbarreta <ende.ciudadjardin@gmail.com>
2024-12-18 08:19:29 +0100

    New outputs to the console

--- prueba2/src/prueba2/Principal.java
+++ prueba2/src/prueba2/Principal.java
@@ -2,12 +2,15 @@ package prueba2;
 
 public class Principal {
 
+
 	public static void main(String[] args) {
 		System.out.println("Hola mundo java-git");
-		System.out.println("Hola de nuevo mundo java-git");
-		
+		System.out.println("Hola mundo java-git");
+		System.out.println("Adios");
+		System.out.println("Pruebas Git. Se añade Cambio1");
 
 	}
+
 
 }
 
